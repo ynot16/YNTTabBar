0d06420efebe9c5acb5e321d87ce0a927e23aa9f
ynot16 <695110728@qq.com>
2016-11-28 17:03:56 +0800

    666

--- BuildScript-gym.sh
+++ BuildScript-gym.sh
@@ -21,7 +21,7 @@ configuration="Release"
 
 #指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, development, 和developer-id，即xcodebuild的method参数
 
-export_method='enterprise'
+export_method="enterprise"
 
 #指定项目地址
 
@@ -61,7 +61,7 @@ echo "===commit msg: $1==="
 
 #先清空前一次build
 
-gym --workspace ${workspace_path} --scheme ${scheme} --clean --use_legacy_build_api --configuration ${configuration} --archive_path ${archive_path} --export_method ${export_method} --output_directory ${output_path} --output_name ${ipa_name}
+gym --workspace ${workspace_path} --scheme ${scheme} --clean --use_legacy_build_api true --configuration ${configuration} --archive_path ${archive_path} --export_method ${export_method} --output_directory ${output_path} --output_name ${ipa_name}
 #gym
 
 # 提交信息
